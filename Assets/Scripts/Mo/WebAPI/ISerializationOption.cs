e176000bea4165d105d84f002777e580997ba127
Constant Dupuis <constant.dupuis@gmail.com>
2022-12-07 14:25:34 +0100

    Add HTTPClient

---
+++ Assets/Scripts/Mo/WebAPI/ISerializationOption.cs
@@ -0,0 +1,10 @@
+﻿namespace Mo.HTTPClient
+{
+    public interface ISerializationOption
+    {
+        string ContentType { get; }
+
+        T Deserialize<T>(string text);
+        string Serialize<T>(T model);
+    }
+}
---
+++ Assets/Scripts/Mo/WebAPI/JsonSerializationOption.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using UnityEngine;
+
+namespace Mo.HTTPClient
+{
+    public class JsonSerializationOption : ISerializationOption
+    {
+        public string ContentType => "application/json";
+
+        public T Deserialize<T>(string text)
+        {
+            var result = JsonConvert.DeserializeObject<T>(text);
+            return result;
+        }
+
+        public string Serialize<T>(T model)
+        {
+            var result = JsonConvert.SerializeObject(model);
+            return result;
+        }
+    }
+}
